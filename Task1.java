748d019b632fb0b50838db1f71bb04ac5e7c2b86
yasyadev <78085638+yasyadev@users.noreply.github.com>
2021-03-29 12:34:44 +0300

    Create Task1.java

---
+++ Task1.java
@@ -0,0 +1,37 @@
+public class Task1 {
+    static volatile char c = 'A';
+    static Object mon = new Object();
+
+    static class WaitNotifyClass implements Runnable {
+        private char currentLetter;
+        private char nextLetter;
+
+        public WaitNotifyClass(char currentLetter, char nextLetter) {
+            this.currentLetter = currentLetter;
+            this.nextLetter = nextLetter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                synchronized (mon) {
+                    try {
+                        while (c != currentLetter)
+                            mon.wait();
+                        System.out.print(currentLetter);
+                        c = nextLetter;
+                        mon.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Task1");
+        new Thread(new WaitNotifyClass('A', 'B')).start();
+        new Thread(new WaitNotifyClass('B', 'C')).start();
+        new Thread(new WaitNotifyClass('C', 'A')).start();
+    }
+}
